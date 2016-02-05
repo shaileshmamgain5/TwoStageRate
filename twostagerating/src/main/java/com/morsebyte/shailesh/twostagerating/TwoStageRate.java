18601bc05a20fc2a329ad7cce15e0065b3dd3547
Shailesh <shailesh@guesthouser.com>
2016-02-05 19:23:45 +0530

    Basic model done

---
+++ twostagerating/src/main/java/com/morsebyte/shailesh/twostagerating/TwoStageRate.java
@@ -0,0 +1,11 @@
+package com.morsebyte.shailesh.twostagerating;
+
+/**
+ * Created by GuestHouser on 2/5/16.
+ */
+public class TwoStageRate {
+    public enum StoreType {
+        GOOGLEPLAY,
+        AMAZON
+    }
+}
